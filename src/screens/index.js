aa3799c4aeddc513ce602434375c2d773d8e70e1
leandrolope <102327675+leandrolope@users.noreply.github.com>
2022-04-14 10:38:34 -0300

    emigrar a react

---
+++ src/screens/index.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import './style.css';
+
+const harry = () => {
+    return (
+    <div>
+         <header>
+       <h1>Harry Potter Characters</h1>
+     </header>
+     <nav>
+       <ul>
+         <li>Name:</li>
+         <h3>Harry Potter</h3>
+         <img src="" alt="picture" />
+         <li>Name:</li>
+         <h3>Hermione Granger</h3>
+         <img src="" alt="picture" />
+       </ul>
+     </nav>
+ 
+     <section>
+         <div>
+
+       <h3>Details About Harry Potter Characters</h3>
+ 
+       <p>Specie</p>
+ 
+       <p><b>Human</b></p>
+ 
+       <p>Gender</p>
+ 
+       <p class="amarillo"><b>Male</b></p>
+ 
+       <p><b>Wand details</b></p>
+ 
+       <p>Wood:</p>
+ 
+       <p><b>Holly</b></p>
+ 
+       <p>Core:</p>
+ 
+       <p><b>Phoenix feather</b></p>
+ 
+       <p>Length</p>
+ 
+       <p><b>11</b></p>
+    </div>
+     </section>
+    </div>
+    )
+}
+
+export default harry
